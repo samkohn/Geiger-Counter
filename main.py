9412ff24da2120f771586bfe93e63bc8503f8ee0
Sam <therunner05@gmail.com>
2012-10-26 16:41:16 -0400

    Added main.py
    has test stuff in it for now

---
+++ main.py
@@ -0,0 +1,11 @@
+###############################
+# Python script for analyzing pra data
+###############################
+
+# Imports
+import scipy as sp
+import matplotlib as ml
+import matplotlib.pyplot as pp
+
+a = sp.arange(10)
+print a
