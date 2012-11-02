45395b19b3e88698620c2f2143e2e1d5b3706bc6
Raymond E <raymond.ehler+git@gmail.com>
2012-11-02 18:36:07 +0000

    added maxTimeResolution

--- analyzeData.py
+++ analyzeData.py
@@ -6,6 +6,8 @@ class DataSet:
     def __init__ (self, count = [], time = []):
         self.counts = count
         self.times = time
+        self.maxTimeResolution = abs(self.times[len(self.times)-1] - self.times[len(self.times)-2])
+        
 
 def readInput(filename):
     with open(filename, 'rb') as csvfile:
@@ -18,10 +20,11 @@ def readInput(filename):
             count.append(row[1])
             time.append(row[0])
     
-    print count
-    print time
-    
+    #for c in counts:
+    #    if c.isdigit() != true
+            
     data = DataSet(count[1:], time[1:])
+    #data = DataSet(count, time)
     
     print data.counts
     print data.times
