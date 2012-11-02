24420cb489870bc05204e903028babadee4c6219
Sam <therunner05@gmail.com>
2012-11-02 15:07:57 -0400

    Rebinning
    added routine rebin_factor from scipy.org and starting to implement in
    plotCountRatePerTime

--- main.py
+++ main.py
@@ -11,16 +11,38 @@ import matplotlib.pyplot as pp
 # Our modules
 import analyzeData as ad
 
+def rebin_factor( a, newshape ):
+    '''Rebin an array to a new shape.
+       newshape must be a factor of a.shape.
+       Source: http://www.scipy.org/Cookbook/Rebinning
+    '''
 
-def plotObject(obj):
-    times = sp.array(obj.times)
-    counts = sp.array(obj.counts)
+    assert len(a.shape) == len(newshape)
+    assert not sometrue(mod( a.shape, newshape ))
+
+    slices = [ slice(None,None, old/new) for old,new in zip(a.shape,newshape) ]
+    return a[slices]
+
+def plotCountRatePerTime(data, timeResolution = None):
+    '''Plot the count rate per time using a given
+    time resolution.  If timeResolution is blank then the
+    default is data.maxTimeResolution.'''
+
+    times = sp.array(data.times)
+    counts = sp.array(data.counts)
+
+    if timeResolution:
+        numBinsToCombine = timeResolution/data.maxTimeResolution
+        
+
+
+
     pp.plot(times, counts)
     pp.show()
 
 def main():
     data = ad.readInput('test.txt')
-    plotObject(data)
+    plotCountRatePerTime(data)
 
     #array = sp.linspace(1,10)
     #print array
