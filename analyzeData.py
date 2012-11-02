344560da2e9771c25eae33689c2e5636c91cbf1c
Raymond E <raymond.ehler+git@gmail.com>
2012-11-02 18:04:55 +0000

    fixed class def and tested it

--- analyzeData.py
+++ analyzeData.py
@@ -4,14 +4,24 @@ import csv
 
 class DataSet:
     def __init__ (self, count = [], time = []):
-        counts = count
-        times = time
+        self.counts = count
+        self.times = time
 
 def readInput(filename):
     with open(filename, 'rb') as csvfile:
         # If a dict is desired, change to csv.DictReader() with the same arguments.
         # However, the code written below will have to be edited to deal with a dictionary
         reader = csv.reader(csvfile, delimiter='\t')
+        count = []
+        time = []
         for row in reader:
-            print row[1]
-        
+            count.append(row[1])
+            time.append(row[0])
+    
+    print count
+    print time
+    
+    data = DataSet(count[1:], time[1:])
+    
+    print data.counts
+    print data.times
