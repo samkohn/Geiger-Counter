3b02220b3755f82fd272880f97178058779c0d69
Raymond E <raymond.ehlers+git@gmail.com>
2012-11-02 20:57:29 +0000

    cleaned up and commented rebin

--- analyzeData.py
+++ analyzeData.py
@@ -9,20 +9,35 @@ class DataSet:
         self.counts = count
         self.times = time
         
-        # maxTimeResolution defines the binWidth (the time between each sampling of the data).
-        # This is a number
+        # maxTimeResolution is a float that defines the binWidth (the time between each sampling of the data).
         self.maxTimeResolution = abs(self.times[len(self.times)-1] - self.times[len(self.times)-2])
     
     def rebin(self, newBinWidth):
         # Determine the factor by which to scale the bins
         rebinningFactor = newBinWidth/self.maxTimeResolution
         
+        # Define new variables to use in the rebinning
         newCounts = []
         newTimes = []
-        for i in range(0,int(len(self.times)/rebinningFactor)):
-            newCounts.append( sum( self.counts[ newBinWidth*i : newBinWidth*(i+1) ] ))
-            newTimes.append(newBinWidth*i)
         
+        # Determine range for i by subtracting the overflow from the length of times.
+        # This does truncate the data, but it is not a full bin, so it doesn't make sense
+        # to count as a bin.
+        overflow = len(self.times) % rebinningFactor
+        upperLimit = (len(self.times) - overflow) / rebinningFactor;
+        
+        for i in range(0, upperLimit):
+            # newTimes are determined by the index times the newBinWidth
+            newTimes.append(newBinWidth*i)
+            
+            # newCounts is the sum of all of the counts in all of the old bins that are
+            # consolidated in the new bin. The bounds are determined by newBinWidth*i and
+            # newBinWidth*(i+1). Further, this number is divided by a rebinningFactor
+            # to ensure that the return result is counts per second.
+            newCount = sum( self.counts[ newBinWidth*i : newBinWidth*(i+1) ] )
+            newCounts.append( newCount / rebinningFactor)
+            
+        # Create a new object to return, so we can keep the original data
         newDataSet = DataSet(newCounts, newTimes)
         
         return newDataSet
