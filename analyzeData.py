0272aa92c0b74e714d2b317efc816f8dcfbb6611
Raymond E <raymond.ehler+git@gmail.com>
2012-11-02 19:12:04 +0000

    rebin() doesn't really work yet, but it is added in

--- analyzeData.py
+++ analyzeData.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python
 
 import csv
+import scipy as sp
 
 class DataSet:
     def __init__ (self, count = [], time = []):
-        self.counts = count
-        self.times = time
+        # Defines counts and times lists, while ensuring that the data being passed in is actually
+        # a number (based on the data read using csv.reader(), these lists are actually strings containing
+        # numbers, but this format of the data works fine with matplotlib).
+        self.counts = [value for value in count if value.isdigit() == True]
+        self.times = [value for value in time if value.isdigit() == True]
+        
+        # maxTimeResolution defines the binWidth (the time between each sampling of the data).
+        # This is a number
         self.maxTimeResolution = abs(float(self.times[len(self.times)-1]) - float(self.times[len(self.times)-2]))
         
-
+    
 def readInput(filename):
     with open(filename, 'rb') as csvfile:
-        # If a dict is desired, change to csv.DictReader() with the same arguments.
-        # However, the code written below will have to be edited to deal with a dictionary
+        # csv.reader returns a list containing strings, from a tab seperated text file.
+        # Based on data taken with PRA 5, the first element in entry of the list it the time,
+        # while the second is the rows
         reader = csv.reader(csvfile, delimiter='\t')
         count = []
         time = []
         for row in reader:
-            count.append(row[1])
             time.append(row[0])
+            count.append(row[1])
     
-    # Remove header strings
-    count = [value for value in count if value.isdigit() == True]
-    time = [value for value in time if value.isdigit() == True]
-            
-    #data = DataSet(count[1:], time[1:])
     data = DataSet(count, time)
     
     print data.counts
     print data.times
     print data.maxTimeResolution
     return data
+
+def rebin( a, newshape):
+    '''Rebin an array to a new shape.
+       newshape must be a factor of a.shape.
+       Source: http://www.scipy.org/Cookbook/Rebinning
+    '''
+
+    assert len(a.shape) == len(newshape)
+    assert not sp.sometrue(sp.mod( a.shape, newshape ))
+
+    slices = [ slice(None,None, old/new) for old,new in zip(a.shape,newshape) ]
+    return a[slices]
