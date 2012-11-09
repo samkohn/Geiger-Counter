078fc73e43ee9890671e2af310a9c4b442014670
Sam <therunner05@gmail.com>
2012-11-09 17:29:39 -0500

    added getCountRates.py

---
+++ getCountRates.py
@@ -0,0 +1,15 @@
+import scipy as sp
+
+def getCountRateArray(self, sampleSize = 1)
+    '''Gets an array with the count rates calculated in each interval
+    of width sampleSize'''
+
+    numBins = int(self.duration / sampleSize)
+
+    # Ignore all times after the last full bin
+    maxTime = numBins * sampleSize
+
+    rates = sp.histogram(self.times, numBins, (0, maxTime))
+
+    print rates
+    return rates
