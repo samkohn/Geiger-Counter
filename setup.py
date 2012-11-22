e27fffdbdbbabf24adaeb3c5e68087a4bf15e59e
Raymond E <raymond.ehlers+git@gmail.com>
2012-11-22 02:48:52 -0500

    Added some setup options, still with no success

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 import py2exe
+import sys
 import matplotlib
+
+sys.path.append("C:\\Users\\rehlers\\Documents\\GitHub\\Geiger-Counter\\dist\\Microsoft.VC90.CRT")
 
 setup(data_files=matplotlib.get_py2exe_datafiles(),windows=["C:\\Users\\rehlers\\Documents\\GitHub\\Geiger-Counter\\GUI.py"])
