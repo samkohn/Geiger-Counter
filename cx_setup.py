aff4809db78e24fa5cd18a682b3e5c9b28b030d9
Raymond E <raymond.ehlers+git@gmail.com>
2012-12-07 01:22:54 -0500

    One last update to docs of cx_setup.py

--- cx_setup.py
+++ cx_setup.py
@@ -1,12 +1,12 @@
 ''' Creates a windows executable with cx_freeze. Run with "python cx_setup build". The package is fairly massive
     right now (~80 MB), partially because it includes extra libraries that are unnecessary, like QT, but one
     must be very careful when removing libraries, as it can cause the whole package to fail. Consequently,
-    they are simply left in.
-'''
+    they are simply left in.'''
 
 import sys
 from cx_Freeze import setup, Executable
 
+# Packages would be excluded here if desired.
 build_exe_options = {
     "includes":[
         "scipy.sparse.csgraph._validation",
@@ -15,12 +15,12 @@ build_exe_options = {
         ]
     }
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
+# GUI applications require a different base on Windows (the default is for a console application).
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+# There are many options here that may be included later if desired.
 setup(
     name = "Geiger-Counter",
     version = "0.1",
